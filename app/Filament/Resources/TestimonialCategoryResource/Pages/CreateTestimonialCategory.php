62fdfb0278745efdb3bfe0718fe76c8b349dc38b
Ayman Elh <elhamioui1964@gmail.com>
2025-06-06 17:49:40 +0100

    feat: make some extra changes on list, create and edit pages

--- app/Filament/Resources/TestimonialCategoryResource/Pages/CreateTestimonialCategory.php
+++ app/Filament/Resources/TestimonialCategoryResource/Pages/CreateTestimonialCategory.php
@@ -9,4 +9,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateTestimonialCategory extends CreateRecord
 {
     protected static string $resource = TestimonialCategoryResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
--- app/Filament/Resources/TestimonialResource/Pages/ListTestimonials.php
+++ app/Filament/Resources/TestimonialResource/Pages/ListTestimonials.php
@@ -13,7 +13,8 @@ class ListTestimonials extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+                ->label('Nouveau Témoignage'),
         ];
     }
 }
